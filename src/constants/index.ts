f48e7c2145541a91ae18da7c1190df5381a3a54f
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-25 18:52:35 +0900

    ♻️ export省略

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
-import { API_URL } from "./API_URL";
-import { LAYOUT } from "./LAYOUT";
-
-export { API_URL, LAYOUT };
+export { API_URL } from "./API_URL";
+export { LAYOUT } from "./LAYOUT";
+export { SUPABASE_PUBLIC_KEY, SUPABASE_URL } from "./SUPABASE";
