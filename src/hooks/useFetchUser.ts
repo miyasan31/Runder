b11ef2f1e671ac91c9fbac817af33152e72be03d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-26 17:25:37 +0900

    ✨ hook:ユーザー一覧を取得

---
+++ src/hooks/useFetchUser.ts
@@ -0,0 +1,51 @@
+import "react-native-url-polyfill/auto";
+
+import type { PostgrestError } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import type { User } from "src/types/fetcher";
+import { supabaseClient } from "src/utils/supabaseClient";
+
+type Status<T> = {
+  isLoading: boolean;
+  isError: PostgrestError | undefined;
+  data: (T[] | null) | undefined;
+};
+
+export const useFetchUser = () => {
+  const [status, setStatus] = useState<Status<User>>({
+    isLoading: true,
+    isError: undefined,
+    data: undefined,
+  });
+
+  const fetch = async () => {
+    try {
+      const { data, error } = await supabaseClient.from<User>("user").select("*");
+
+      if (error) {
+        setStatus((prev) => {
+          return { ...prev, isLoading: false, isError: error };
+        });
+      }
+
+      setStatus((prev) => {
+        if (!data || !data.length) {
+          return { ...prev, isLoading: false, data: null };
+        }
+        return { ...prev, isLoading: false, data: data };
+      });
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  return {
+    isLoading: status.isLoading,
+    isError: status.isError,
+    data: status.data,
+  };
+};
