cd1d96c1fe30c3c1e94ea439cd4921dc4a5aac35
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-26 17:20:12 +0900

    ✨ model.userの作成

--- src/types/fetcher.d.ts
+++ src/types/fetcher.d.ts
@@ -4,17 +4,5 @@
 export type User = {
   id: number;
   name: string;
-  age: number;
-};
-
-/**
- * Model Post
- */
-export type Post = {
-  id: number;
-  title: string;
-  body: string;
-  createdAt: Date;
-  updatedAt: Date;
-  User: User;
+  created_at: Date;
 };
