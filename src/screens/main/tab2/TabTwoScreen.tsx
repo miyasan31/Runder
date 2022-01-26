c6c7e5ad29243e16c7f5d95a722025d35d243ccf
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-26 17:29:27 +0900

    ✅ useSupabaseの使用

--- src/screens/main/tab2/TabTwoScreen.tsx
+++ src/screens/main/tab2/TabTwoScreen.tsx
@@ -1,50 +1,56 @@
+import "react-native-url-polyfill/auto";
+
 import { format } from "date-fns";
 import type { VFC } from "react";
 import React from "react";
 import { FlatList, StyleSheet } from "react-native";
-import { ListItem, Progress } from "src/components";
+import { ListItem } from "src/components";
 import { Text, View } from "src/components/custom";
 import { SafeAreaLayout } from "src/components/layout";
-import { useGetSWRdev, useThemeColor } from "src/hooks";
-import type { TabTwoScreenProps } from "types";
-import type { Post } from "types/fetcher";
+import { useSupabaseFilter, useSupabaseSelect, useThemeColor } from "src/hooks";
+import type { TabTwoScreenProps } from "src/types";
+import type { User } from "src/types/fetcher";
 
 export const TabTwoScreen: VFC<TabTwoScreenProps<"TabTwoScreen">> = () => {
   const color = useThemeColor({}, "text2");
-  const { data, isError, isLoading } = useGetSWRdev<Post[]>("/post");
 
-  const renderItem = ({ item }: { item: Post }) => {
-    const onNavigation = () => {
-      console.info("item.id", item.id);
-    };
+  const filter = useSupabaseFilter((query) => query.eq("name", "みやさん"), []);
+  const { loading, error, data } = useSupabaseSelect<User>("user", {
+    options: {
+      count: "exact",
+    },
+    filter,
+  });
 
-    const date = format(new Date(item.updatedAt), "yyyy年M月d日");
+  return (
+    <SafeAreaLayout>
+      {loading ? (
+        <Text>読み込み中...</Text>
+      ) : error ? (
+        <Text>エラーが発生しました</Text>
+      ) : !data ? (
+        <Text>データが存在しません</Text>
+      ) : (
+        <FlatList data={data} renderItem={renderItem} keyExtractor={(item, _) => String(item.id)} />
+      )}
+    </SafeAreaLayout>
+  );
 
+  // eslint-disable-next-line func-style
+  function renderItem({ item }: { item: User }) {
+    const date = format(new Date(item.created_at), "yyyy年M月d日");
+    const onNavigation = () => console.info("item.id", item.id);
     return (
       <ListItem style={styles.list} onPress={onNavigation}>
         <View>
-          <Text style={styles.shopName}>{item.title}</Text>
+          <Text style={styles.shopName}>{item.name}</Text>
           <Text style={styles.date} lightTextColor={color} darkTextColor={color}>
             {date}
           </Text>
         </View>
       </ListItem>
     );
-  };
-
-  return (
-    <SafeAreaLayout>
-      {isLoading ? (
-        <Progress />
-      ) : isError ? (
-        <Text>エラーが発生しました</Text>
-      ) : !data ? (
-        <Text>データがありません</Text>
-      ) : (
-        <FlatList data={data} renderItem={renderItem} keyExtractor={(item, _) => String(item.id)} />
-      )}
-    </SafeAreaLayout>
-  );
+  }
 };
 
 const styles = StyleSheet.create({
