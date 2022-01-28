62f4750537ec1bf58fc61f9f5ce3b8da84708bf0
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-29 01:42:02 +0900

    🏷️ Location

--- src/types/fetcher.d.ts
+++ src/types/fetcher.d.ts
@@ -6,3 +6,24 @@ export type User = {
   name: string;
   created_at: Date;
 };
+
+/**
+ * Model Location
+ */
+export type Location = {
+  id: number;
+  location: LocationResult[];
+  created_at: Date;
+};
+
+export type LocationResult = {
+  accuracy: number | null;
+  altitude: number | null;
+  altitudeAccuracy: number | null;
+  heading: number | null;
+  latitude: number;
+  longitude: number;
+  speed: number | null;
+  timestamp: number | null;
+  color: string;
+};
