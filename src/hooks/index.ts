cdf42809c01a3fe9ccd72b1d91d150098a4b7ca5
miyasan_0301 <miyasan.19990301@gmail.com>
2022-01-29 01:55:17 +0900

    🔥 swr関連

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,5 @@
 export { useCachedResources } from "./useCachedResources";
 export { useColorScheme } from "./useColorScheme";
 export { useFetchUser } from "./useFetchUser";
-export { useGetSWR } from "./useGetSWR";
-export { useGetSWRdev } from "./useGetSWRdev";
 export { useSupabaseFilter, useSupabaseSelect } from "./useSupabase";
 export { useThemeColor } from "./useThemeColor";
