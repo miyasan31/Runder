0df7bcaa69fd49fb67a9c78e6db664370749a091
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-04 17:26:52 +0900

    ✨ auth0

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export { API_URL } from "./API_URL";
+export { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "./AUTH0";
 export { LAYOUT } from "./LAYOUT";
 export { SUPABASE_PUBLIC_KEY, SUPABASE_URL } from "./SUPABASE";
