76840ccdd7b6f7f265fd4715e6dff5dedfd9559d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-04 13:00:02 +0900

    🚧 認証処理

--- src/screens/auth/SigninScreen.tsx
+++ src/screens/auth/SigninScreen.tsx
@@ -1,29 +1,27 @@
-import { makeRedirectUri, startAsync } from "expo-auth-session";
+// import { makeRedirectUri, startAsync } from "expo-auth-session";
 import type { VFC } from "react";
 import React from "react";
 import { Text, View } from "react-native";
 
 import { ColorButton } from "~/components/custom";
-import { SUPABASE_URL } from "~/constants";
+// import { SUPABASE_URL } from "~/constants";
 import type { AuthScreenProps } from "~/types";
-import { supabaseClient } from "~/utils/supabaseClient";
+// import { supabaseClient } from "~/utils/supabaseClient";
 
 export const SigninScreen: VFC<AuthScreenProps<"SigninScreen">> = () => {
   const onGoogleSignin = async () => {
-    const redirectUri = makeRedirectUri({ path: "/", useProxy: false });
-    const provider = "google";
-
-    startAsync({
-      authUrl: `${SUPABASE_URL}/auth/v1/authorize?provider=${provider}&redirect_to=${redirectUri}`,
-      returnUrl: redirectUri,
-    }).then(async (response: any) => {
-      if (!response) return;
-
-      const { user, session, error } = await supabaseClient.auth.signIn({
-        refreshToken: response.params?.refresh_token,
-      });
-      console.info(user, session, error);
-    });
+    // const redirectUri = makeRedirectUri({ path: "/", useProxy: false });
+    // const provider = "google";
+    // startAsync({
+    //   authUrl: `${SUPABASE_URL}/auth/v1/authorize?provider=${provider}&redirect_to=${redirectUri}`,
+    //   returnUrl: redirectUri,
+    // }).then(async (response: any) => {
+    //   if (!response) return;
+    //   const { user, session, error } = await supabaseClient.auth.signIn({
+    //     refreshToken: response.params?.refresh_token,
+    //   });
+    //   console.info(user, session, error);
+    // });
   };
 
   return (
