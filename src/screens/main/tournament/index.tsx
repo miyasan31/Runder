f3af980b9e19357d83f7807c8d85e3e1cf93c7f5
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 11:10:36 +0900

    🔧 headerShown: false

--- src/screens/main/tournament/index.tsx
+++ src/screens/main/tournament/index.tsx
@@ -1,28 +1,26 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import type { VFC } from "react";
-import React from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { VFC } from 'react';
+import React from 'react';
 
-import { useThemeColor } from "~/hooks/useThemeColor";
-import type { TounamentStackParamList } from "~/types";
+import type { TounamentStackParamList } from '~/types';
 
-import { ModalScreen } from "./hoge.modal";
-import { TounamentScreen } from "./tounament.screen";
+import { ModalScreen } from './hoge.modal';
+import { TounamentScreen } from './tounament.screen';
 
 const Tounament = createNativeStackNavigator<TounamentStackParamList>();
 
 export const TounamentNavigator: VFC = () => {
-  const backgroundColor = useThemeColor({}, "bg1");
-
   return (
     <Tounament.Navigator
       initialRouteName="TounamentScreen"
       screenOptions={{
-        headerStyle: { backgroundColor: backgroundColor },
+        headerShown: false,
       }}
     >
       <Tounament.Screen name="TounamentScreen" component={TounamentScreen} options={() => ({})} />
-      <Tounament.Group screenOptions={{ presentation: "modal" }}>
-        <Tounament.Screen name="Modal" component={ModalScreen} options={{ title: "Oops!" }} />
+
+      <Tounament.Group screenOptions={{ presentation: 'modal' }}>
+        <Tounament.Screen name="Modal" component={ModalScreen} options={{ title: 'Oops!' }} />
       </Tounament.Group>
     </Tounament.Navigator>
   );
