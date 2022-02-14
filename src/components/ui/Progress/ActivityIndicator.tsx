5707d62db2f9bf522dff142c0f6d11c6722c6a59
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 13:27:08 +0900

    ✨ ActivityIndicator

--- src/components/ui/Progress/ActivityIndicator.tsx
+++ src/components/ui/Progress/ActivityIndicator.tsx
@@ -1,12 +1,12 @@
-import type { VFC } from "react";
-import React from "react";
-import { ActivityIndicator as NativeActivityIndicator, StyleSheet } from "react-native";
+import type { VFC } from 'react';
+import React from 'react';
+import { ActivityIndicator as NativeActivityIndicator, StyleSheet } from 'react-native';
 
-import { View } from "~/components/ui/View";
-import { useThemeColor } from "~/hooks/useThemeColor";
+import { View } from '~/components/ui/View';
+import { useThemeColor } from '~/hooks/useThemeColor';
 
 export const ActivityIndicator: VFC = () => {
-  const primary = useThemeColor({}, "primary");
+  const primary = useThemeColor({}, 'primary');
 
   return (
     <View style={defaultStyle.center}>
@@ -18,7 +18,7 @@ export const ActivityIndicator: VFC = () => {
 const defaultStyle = StyleSheet.create({
   center: {
     flex: 1,
-    width: "100%",
-    alignItems: "center",
+    width: '100%',
+    alignItems: 'center',
   },
 });
