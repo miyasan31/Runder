d26427e49b3c28abc2493f11ed1562d9a5fb884c
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 11:46:05 +0900

    ✨ DevOpsScreen

---
+++ src/components/screen/Contact/DevOps.screen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { LayoutErrorBoundary } from '~/components/functional/Error';
+import { Layout } from '~/components/ui/Layout';
+
+import { DevOps } from './DevOps';
+import type { ContactScreenProps } from './ScreenProps';
+
+export const DevOpsScreen: ContactScreenProps = (props) => {
+  return (
+    <LayoutErrorBoundary>
+      <Layout>
+        <DevOps {...props} />
+      </Layout>
+    </LayoutErrorBoundary>
+  );
+};
