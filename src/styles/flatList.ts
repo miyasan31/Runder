d779e65a7c1b3bf47131f824bb9757d63977ac09
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 16:20:33 +0900

    ✨ Create a flatListStyle

---
+++ src/styles/flatList.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const flatListStyle = StyleSheet.create({
+  list: {
+    width: '100%',
+  },
+  container: {
+    paddingTop: '5%',
+    paddingBottom: 150,
+  },
+});
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,6 @@
-export { buttonStyles } from "./button";
-export { textStyles } from "./text";
-export { textInputStyles } from "./textInput";
-export { theme } from "./theme";
-export { viewStyles } from "./view";
+export { buttonStyles } from './button';
+export { flatListStyle } from './flatList';
+export { textStyles } from './text';
+export { textInputStyles } from './textInput';
+export { theme } from './theme';
+export { viewStyles } from './view';
