bf5f70339b9ef98fc86acc1f94488c4d771bcf96
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 13:07:40 +0900

    ✨ useColorScheme

--- src/hooks/useColorScheme.ts
+++ src/hooks/useColorScheme.ts
@@ -1,5 +1,5 @@
-import type { ColorSchemeName } from "react-native";
-import { useColorScheme as NativeUseColorScheme } from "react-native";
+import type { ColorSchemeName } from 'react-native';
+import { useColorScheme as NativeUseColorScheme } from 'react-native';
 
 export const useColorScheme = (): NonNullable<ColorSchemeName> => {
   return NativeUseColorScheme() as NonNullable<ColorSchemeName>;
