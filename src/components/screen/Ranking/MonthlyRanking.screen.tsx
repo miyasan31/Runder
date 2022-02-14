1a1e085bd6f6231bbe27c324211b1ed12f171de6
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 11:30:54 +0900

    ✨ MonthlyRankingScreen

---
+++ src/components/screen/Ranking/MonthlyRanking.screen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { LayoutErrorBoundary } from '~/components/functional/Error';
+import { Layout } from '~/components/ui/Layout';
+
+import { MonthlyRanking } from './MonthlyRanking';
+import type { RankingScreenProps } from './ScreenProps';
+
+export const MonthlyRankingScreen: RankingScreenProps = (props) => {
+  return (
+    <LayoutErrorBoundary>
+      <Layout>
+        <MonthlyRanking {...props} />
+      </Layout>
+    </LayoutErrorBoundary>
+  );
+};
