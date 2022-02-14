844cea84f98f128c72de7940b4be649db2d6e293
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 11:08:22 +0900

    ✨ TabViewProvider

---
+++ src/components/functional/TabViewProvider/TabViewProvider.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode, VFC } from 'react';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { SafeAreaView, View } from '~/components/ui/View';
+import type { ViewStyleProps } from '~/types/style';
+
+type LayoutProps = ViewStyleProps & {
+  children: ReactNode;
+};
+
+export const TabViewProvider: VFC<LayoutProps> = ({
+  // 基本的に使用しない
+  lightBg,
+  darkBg,
+  // custome theme
+  bgTheme = 'bg1',
+  // ViewProps
+  bgStyle,
+  children,
+}) => {
+  return (
+    <SafeAreaView
+      edges={['top', 'left', 'right']}
+      style={defaultStyle.root}
+      {...{ lightBg, darkBg, bgTheme, bgStyle }}
+    >
+      <View style={defaultStyle.root}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+const defaultStyle = StyleSheet.create({
+  root: {
+    flex: 1,
+    position: 'relative',
+  },
+});
---
+++ src/components/functional/TabViewProvider/index.ts
@@ -0,0 +1,1 @@
+export { TabViewProvider } from './TabViewProvider';
