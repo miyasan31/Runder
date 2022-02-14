2b4a302b3b72bbd7fd411b41166496ebbba4ff71
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 16:23:47 +0900

    🚧 VirtualTournament

--- src/components/screen/Tournament/VirtualTournament.tsx
+++ src/components/screen/Tournament/VirtualTournament.tsx
@@ -1,69 +1,56 @@
 import 'react-native-url-polyfill/auto';
 
-import { format } from 'date-fns';
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
 
-import { Card } from '~/components/ui/Card';
-import { Progress } from '~/components/ui/Progress';
-import { Text } from '~/components/ui/Text';
-import { View } from '~/components/ui/View';
-import { useSupabaseFilter, useSupabaseSelect } from '~/hooks/useSupabase';
-import type { User } from '~/types/fetcher';
+import { TournamentCard } from '~/components/model/Tournament/TournamentCard';
+import { flatListStyle } from '~/styles';
 
 import type { TournamentScreenProps } from './ScreenProps';
 
+const data = [
+  {
+    id: '1',
+    name: 'Winter Distance Challenge',
+    distance: 3000,
+    created_at: '2020-01-01',
+    image: './assets/develop/tournament.jpeg',
+  },
+  {
+    id: '2',
+    name: 'Winter Distance Challenge',
+    distance: 1000,
+    created_at: '2020-01-01',
+    image: './assets/develop/tournament.jpeg',
+  },
+  {
+    id: '3',
+    name: 'Winter Distance Challenge',
+    distance: 1000,
+    created_at: '2020-01-01',
+    image: './assets/develop/tournament.jpeg',
+  },
+  {
+    id: '4',
+    name: 'Winter Distance Challenge',
+    distance: 1000,
+    created_at: '2020-01-01',
+    image: './assets/develop/tournament.jpeg',
+  },
+];
+
+type Tournament = typeof data[0];
+
 export const VirtualTournament: TournamentScreenProps = () => {
-  const filter = useSupabaseFilter((query) => query.limit(10), []);
-  const { loading, error, data } = useSupabaseSelect<User>('user', {
-    options: {
-      count: 'exact',
-    },
-    filter,
-  });
-
-  if (loading) return <Progress />;
-  if (error) return <Text>エラー</Text>;
-  if (!data) return <Text>データなし</Text>;
-
   return (
-    <FlatList data={data} renderItem={renderItem} keyExtractor={(item, _) => String(item.id)} />
+    <FlatList
+      data={data}
+      style={flatListStyle.list}
+      contentContainerStyle={flatListStyle.container}
+      keyExtractor={(item, _) => String(item.id)}
+      renderItem={({ item }: { item: Tournament }) => {
+        return <TournamentCard {...item} />;
+      }}
+    />
   );
-
-  // eslint-disable-next-line func-style
-  function renderItem({ item }: { item: User }) {
-    const date = format(new Date(item.created_at), 'yyyy年M月d日');
-    const onNavigation = () => console.info('item.id', item.id);
-
-    return (
-      <Card bgStyle={style.list} isBorder onPress={onNavigation}>
-        <View>
-          <Text style={style.shopName}>{item.name}</Text>
-          <Text textTheme="text2" style={style.date}>
-            {date}
-          </Text>
-        </View>
-      </Card>
-    );
-  }
 };
-
-const style = StyleSheet.create({
-  list: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 18,
-    marginVertical: '1%',
-  },
-  shopName: {
-    paddingBottom: 10,
-    fontSize: 20,
-    textAlign: 'left',
-  },
-  date: {
-    fontSize: 15,
-    fontWeight: 'normal',
-    textAlign: 'left',
-  },
-});
