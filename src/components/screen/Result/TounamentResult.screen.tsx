af67cba19917b61ceae0d4255692a4cfaa109ebd
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-14 11:55:56 +0900

    ✨ TounamentResultScreen

---
+++ src/components/screen/Result/TounamentResult.screen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { LayoutErrorBoundary } from '~/components/functional/Error';
+import { Layout } from '~/components/ui/Layout';
+
+import type { ResultScreenProps } from './ScreenProps';
+import { TounamentResult } from './TounamentResult';
+
+export const TounamentResultScreen: ResultScreenProps = (props) => {
+  return (
+    <LayoutErrorBoundary>
+      <Layout>
+        <TounamentResult {...props} />
+      </Layout>
+    </LayoutErrorBoundary>
+  );
+};
---
+++ src/components/screen/Result/TounamentResult.tsx
@@ -0,0 +1,69 @@
+import 'react-native-url-polyfill/auto';
+
+import { format } from 'date-fns';
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+
+import { Card } from '~/components/ui/Card';
+import { Progress } from '~/components/ui/Progress';
+import { Text } from '~/components/ui/Text';
+import { View } from '~/components/ui/View';
+import { useSupabaseFilter, useSupabaseSelect } from '~/hooks/useSupabase';
+import type { User } from '~/types/fetcher';
+
+import type { ResultScreenProps } from './ScreenProps';
+
+export const TounamentResult: ResultScreenProps = () => {
+  const filter = useSupabaseFilter((query) => query.limit(10), []);
+  const { loading, error, data } = useSupabaseSelect<User>('user', {
+    options: {
+      count: 'exact',
+    },
+    filter,
+  });
+
+  if (loading) return <Progress />;
+  if (error) return <Text>エラー</Text>;
+  if (!data) return <Text>データなし</Text>;
+
+  return (
+    <FlatList data={data} renderItem={renderItem} keyExtractor={(item, _) => String(item.id)} />
+  );
+
+  // eslint-disable-next-line func-style
+  function renderItem({ item }: { item: User }) {
+    const date = format(new Date(item.created_at), 'yyyy年M月d日');
+    const onNavigation = () => console.info('item.id', item.id);
+
+    return (
+      <Card bgStyle={style.list} isBorder onPress={onNavigation}>
+        <View>
+          <Text style={style.shopName}>{item.name}</Text>
+          <Text textTheme="text2" style={style.date}>
+            {date}
+          </Text>
+        </View>
+      </Card>
+    );
+  }
+};
+
+const style = StyleSheet.create({
+  list: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 18,
+    marginVertical: '1%',
+  },
+  shopName: {
+    paddingBottom: 10,
+    fontSize: 20,
+    textAlign: 'left',
+  },
+  date: {
+    fontSize: 15,
+    fontWeight: 'normal',
+    textAlign: 'left',
+  },
+});
