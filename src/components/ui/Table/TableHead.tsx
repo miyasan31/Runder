7075bc12c2718aa43deb5a339c9576e3c514b932
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-17 21:33:14 +0900

    ✨ TableHead

---
+++ src/components/ui/Table/TableHead.tsx
@@ -0,0 +1,37 @@
+import type { VFC } from 'react';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Text } from '~/components/ui/Text';
+import { View } from '~/components/ui/View';
+
+type Props = {
+  leftTitle: string;
+  rightTitle: string;
+};
+
+export const TableHead: VFC<Props> = ({ leftTitle, rightTitle }) => {
+  return (
+    <View style={style.th} bgTheme="bg2">
+      <Text style={style.thRank}>{leftTitle}</Text>
+      <Text style={style.thPoint}>{rightTitle}</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  th: {
+    flexDirection: 'row',
+    padding: '4%',
+    borderRadius: 10,
+  },
+  thRank: {
+    flex: 1,
+    fontWeight: 'bold',
+  },
+  thPoint: {
+    flex: 1,
+    textAlign: 'right',
+    fontWeight: 'bold',
+  },
+});
