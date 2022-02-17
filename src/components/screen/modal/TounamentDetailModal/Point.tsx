e4960b771aabb403a26c63b62da84ce65b717835
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-17 18:22:34 +0900

    🚧 Tournament Detail Content

---
+++ src/components/screen/modal/TounamentDetailModal/Point.tsx
@@ -0,0 +1,13 @@
+import type { VFC } from 'react';
+import React from 'react';
+
+import { Text } from '~/components/ui/Text';
+import { View } from '~/components/ui/View';
+
+export const Point: VFC = () => {
+  return (
+    <View>
+      <Text>Point</Text>
+    </View>
+  );
+};
