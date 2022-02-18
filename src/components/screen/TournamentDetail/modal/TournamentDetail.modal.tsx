e691ecc4f85865f9399de31b60d68b3e875f5d78
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-18 16:25:17 +0900

    🐛 Re-rendering with modal animation

--- src/components/screen/TournamentDetail/modal/TournamentDetail.modal.tsx
+++ src/components/screen/TournamentDetail/modal/TournamentDetail.modal.tsx
@@ -1,19 +1,8 @@
 import type { VFC } from 'react';
-import { SceneMap, TabView } from 'react-native-tab-view';
 
 import { HalfModal } from '~/components/ui/Modal';
-import { TabBar } from '~/components/ui/TabBar';
-import { useTabView } from '~/hooks/useTabView';
 
-import { Point } from './Point';
-import { Ranking } from './Ranking';
-import { Rule } from './Rule';
-
-const routes = [
-  { key: 'rule', title: 'ルール' },
-  { key: 'point', title: 'ポイント' },
-  { key: 'ranking', title: 'ランキング' },
-];
+import { TournamentDetailTab } from './TournamentDetail.tab';
 
 type Props = {
   isVisible: boolean;
@@ -22,21 +11,9 @@ type Props = {
 };
 
 export const TournamentDetailModal: VFC<Props> = (props) => {
-  const { layout, index, onIndexChange } = useTabView(props.activeTabIndex);
-
   return (
     <HalfModal {...props} size={0.7}>
-      <TabView
-        renderTabBar={TabBar}
-        navigationState={{ index, routes }}
-        onIndexChange={onIndexChange}
-        initialLayout={{ width: layout.width }}
-        renderScene={SceneMap({
-          rule: () => <Rule />,
-          point: () => <Point />,
-          ranking: () => <Ranking />,
-        })}
-      />
+      <TournamentDetailTab activeTabIndex={props.activeTabIndex} />
     </HalfModal>
   );
 };
---
+++ src/components/screen/TournamentDetail/modal/TournamentDetail.tab.tsx
@@ -0,0 +1,37 @@
+import type { VFC } from 'react';
+import { SceneMap, TabView } from 'react-native-tab-view';
+
+import { TabBar } from '~/components/ui/TabBar';
+import { useTabView } from '~/hooks/useTabView';
+
+import { Point } from './Point';
+import { Ranking } from './Ranking';
+import { Rule } from './Rule';
+
+const routes = [
+  { key: 'rule', title: 'ルール' },
+  { key: 'point', title: 'ポイント' },
+  { key: 'ranking', title: 'ランキング' },
+];
+
+type Props = {
+  activeTabIndex?: number;
+};
+
+export const TournamentDetailTab: VFC<Props> = (props) => {
+  const { layout, index, onIndexChange } = useTabView(props.activeTabIndex);
+
+  return (
+    <TabView
+      renderTabBar={TabBar}
+      navigationState={{ index, routes }}
+      onIndexChange={onIndexChange}
+      initialLayout={{ width: layout.width }}
+      renderScene={SceneMap({
+        rule: () => <Rule />,
+        point: () => <Point />,
+        ranking: () => <Ranking />,
+      })}
+    />
+  );
+};
