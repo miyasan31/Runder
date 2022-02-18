51c4aeb2238a4f38c37fb48d607cd8ef65415554
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-18 12:20:32 +0900

    ✨ Component Integration

--- src/components/screen/TournamentDetail/modal/TournamentDetail.modal.tsx
+++ src/components/screen/TournamentDetail/modal/TournamentDetail.modal.tsx
@@ -1,8 +1,19 @@
 import type { VFC } from 'react';
+import { SceneMap, TabView } from 'react-native-tab-view';
 
 import { HalfModal } from '~/components/ui/Modal';
+import { TabBar } from '~/components/ui/TabBar';
+import { useTabView } from '~/hooks/useTabView';
 
-import { TournamentDetailModalTab } from './TournamentDetailModal.tab';
+import { Point } from './Point';
+import { Ranking } from './Ranking';
+import { Rule } from './Rule';
+
+const routes = [
+  { key: 'rule', title: 'ルール' },
+  { key: 'point', title: 'ポイント' },
+  { key: 'ranking', title: 'ランキング' },
+];
 
 type Props = {
   isVisible: boolean;
@@ -11,9 +22,21 @@ type Props = {
 };
 
 export const TournamentDetailModal: VFC<Props> = (props) => {
+  const { layout, index, onIndexChange } = useTabView(props.activeTabIndex);
+
   return (
     <HalfModal {...props} size={0.7}>
-      <TournamentDetailModalTab activeTabIndex={props.activeTabIndex} />
+      <TabView
+        renderTabBar={TabBar}
+        navigationState={{ index, routes }}
+        onIndexChange={onIndexChange}
+        initialLayout={{ width: layout.width }}
+        renderScene={SceneMap({
+          rule: () => <Rule />,
+          point: () => <Point />,
+          ranking: () => <Ranking />,
+        })}
+      />
     </HalfModal>
   );
 };
