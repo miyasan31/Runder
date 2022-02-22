4b31385dc78fa998b9f57cc5c0096108d6e7c70c
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-22 17:50:19 +0900

    💄 justifyContent: center

--- src/components/ui/Progress/ActivityIndicator.tsx
+++ src/components/ui/Progress/ActivityIndicator.tsx
@@ -1,24 +1,24 @@
 import type { VFC } from 'react';
-import React from 'react';
+import React, { memo } from 'react';
 import { ActivityIndicator as NativeActivityIndicator, StyleSheet } from 'react-native';
 
 import { View } from '~/components/ui/View';
 import { useThemeColor } from '~/hooks/useThemeColor';
 
-export const ActivityIndicator: VFC = () => {
+export const ActivityIndicator: VFC = memo(() => {
   const primary = useThemeColor({}, 'primary');
 
   return (
-    <View style={defaultStyle.center}>
+    <View style={defaultStyle.center} bgTheme="bg1">
       <NativeActivityIndicator size="large" color={primary} />
     </View>
   );
-};
+});
 
 const defaultStyle = StyleSheet.create({
   center: {
     flex: 1,
-    width: '100%',
     alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- src/components/ui/Progress/Progress.tsx
+++ src/components/ui/Progress/Progress.tsx
@@ -1,12 +1,12 @@
 import type { VFC } from 'react';
-import React from 'react';
+import React, { memo } from 'react';
 import { StyleSheet } from 'react-native';
 import * as RNProgress from 'react-native-progress';
 
 import { View } from '~/components/ui/View';
 import { useThemeColor } from '~/hooks/useThemeColor';
 
-export const Progress: VFC = () => {
+export const Progress: VFC = memo(() => {
   const primary = useThemeColor({}, 'primary');
 
   return (
@@ -14,12 +14,12 @@ export const Progress: VFC = () => {
       <RNProgress.CircleSnail color={primary} size={50} thickness={4} spinDuration={800} />
     </View>
   );
-};
+});
 
 const defaultStyle = StyleSheet.create({
   center: {
     flex: 1,
-    width: '100%',
     alignItems: 'center',
+    justifyContent: 'center',
   },
 });
