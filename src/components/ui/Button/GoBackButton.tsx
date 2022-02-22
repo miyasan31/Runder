4ed30900da4b247faec8797a09fe02d0c532fc48
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-23 00:54:52 +0900

    ✨ GoBackButton

---
+++ src/components/ui/Button/GoBackButton.tsx
@@ -0,0 +1,32 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import type { VFC } from 'react';
+import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+import { Bounceable } from 'rn-bounceable';
+
+import { View } from '~/components/ui/View';
+import { useThemeColor } from '~/hooks/useThemeColor';
+
+type Props = {
+  onPress: () => void;
+};
+
+export const GoBackButton: VFC<Props> = memo(({ onPress }) => {
+  const icon = useThemeColor({}, 'icon');
+
+  return (
+    <View style={style.floatButton}>
+      <Bounceable onPress={onPress} activeScale={0.9}>
+        <MaterialIcons name="keyboard-arrow-left" size={35} color={icon} />
+      </Bounceable>
+    </View>
+  );
+});
+
+const style = StyleSheet.create({
+  floatButton: {
+    position: 'absolute',
+    top: '1%',
+    left: '4%',
+  },
+});
