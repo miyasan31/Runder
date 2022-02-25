2fc30ceaf1da274b2fee1327606d094d762930a9
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-25 15:02:16 +0900

    💄 toast

--- src/utils/toastKit.ts
+++ src/utils/toastKit.ts
@@ -1,14 +1,14 @@
 /* eslint-disable no-shadow */
 import { toast } from 'react-hot-toast/src/core/toast';
 
-export const toastKit = (loadingText = '処理中...', { icon = '💁‍♂️' } = {}) => {
+export const toastKit = (loadingText = '処理中...', { icon = '🤔' } = {}) => {
   const id = toast.loading(loadingText, {
     icon,
   });
 
   const errorToast = (
     errorText = 'エラーが発生しました',
-    { duration = 4000, icon = '🙅‍♂️' } = {},
+    { duration = 4000, icon = '😱' } = {},
   ) => {
     return toast.error(errorText, {
       id,
@@ -17,7 +17,7 @@ export const toastKit = (loadingText = '処理中...', { icon = '💁‍♂️' 
     });
   };
 
-  const successToast = (successText: string, { duration = 2000, icon = '🙆‍♂️' } = {}) => {
+  const successToast = (successText: string, { duration = 2000, icon = '🥳' } = {}) => {
     return toast.success(successText, {
       id,
       icon,
