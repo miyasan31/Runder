43e514979efc8ceded719fd3fa2405d19230ff0d
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-25 15:03:09 +0900

    ✨ onSignUpEmail

--- src/utils/supabase/index.ts
+++ src/utils/supabase/index.ts
@@ -1,4 +1,5 @@
 export { onSignInGoogle } from './onSignInGoogle';
 export { onSignOut } from './onSignOut';
+export { onSignUpEmail } from './onSignUpEmail';
 export { supabaseClient } from './supabaseClient';
 export { supabaseSelect } from './supabaseSelect';
