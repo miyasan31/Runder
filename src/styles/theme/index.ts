26d4ccaff5c94c63f18e9bd00e4d404876878157
miyasan_0301 <miyasan.19990301@gmail.com>
2022-02-26 22:47:34 +0900

    🏷️ Same key check for light and dark.

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -10,19 +10,55 @@ export type ColorTheme = keyof typeof colorTheme[keyof typeof colorTheme];
 export type IconTheme = keyof typeof iconTheme[keyof typeof iconTheme];
 export type BorderTheme = keyof typeof borderTheme[keyof typeof borderTheme];
 
-export const theme = {
-  light: {
-    ...commonTheme.light,
-    ...bgTheme.light,
-    ...colorTheme.light,
-    ...iconTheme.light,
-    ...borderTheme.light,
-  },
-  dark: {
-    ...commonTheme.dark,
-    ...bgTheme.dark,
-    ...colorTheme.dark,
-    ...iconTheme.dark,
-    ...borderTheme.dark,
-  },
-} as const;
+// Themeのlightとdarkのkeyが同じものがあることを確認する
+type Theme = Record<'light' | 'dark', Record<string, string>>;
+type ThemeCheck<T extends Theme> = Record<
+  'light' | 'dark',
+  Record<keyof T['light'] | keyof T['dark'], string>
+>;
+
+export const createTheme = () => {
+  const common: ThemeCheck<typeof commonTheme> = commonTheme;
+  const color: ThemeCheck<typeof colorTheme> = colorTheme;
+  const bg: ThemeCheck<typeof bgTheme> = bgTheme;
+  const border: ThemeCheck<typeof borderTheme> = borderTheme;
+  const icon: ThemeCheck<typeof iconTheme> = iconTheme;
+
+  return {
+    light: {
+      ...common.light,
+      ...color.light,
+      ...bg.light,
+      ...border.light,
+      ...icon.light,
+    },
+    dark: {
+      ...common.dark,
+      ...color.dark,
+      ...bg.dark,
+      ...border.dark,
+      ...icon.dark,
+    },
+  };
+};
+
+export const theme = createTheme();
+
+// Themeのkeyについて確認なし
+
+// export const theme = {
+//   light: {
+//     ...commonTheme.light,
+//     ...bgTheme.light,
+//     ...colorTheme.light,
+//     ...iconTheme.light,
+//     ...borderTheme.light,
+//   },
+//   dark: {
+//     ...commonTheme.dark,
+//     ...bgTheme.dark,
+//     ...colorTheme.dark,
+//     ...iconTheme.dark,
+//     ...borderTheme.dark,
+//   },
+// } as const;
