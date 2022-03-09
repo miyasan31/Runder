fd5973153b0069d63d6f8571bb06b42ca6c94102
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-09 19:35:45 +0900

    💄 theme bg: bg2

--- src/styles/theme/bg.theme.tsx
+++ src/styles/theme/bg.theme.tsx
@@ -2,14 +2,14 @@ export const bgTheme = {
   light: {
     bg0: '#ffffff00',
     bg1: '#FFFFFF',
-    bg2: '#EEEEEE',
+    bg2: '#F5F5F5',
     bg3: '#DDDDDD',
     bg4: '#FAFAFA',
   },
   dark: {
     bg0: '#00000000',
     bg1: '#373c3f',
-    bg2: '#2f3437',
+    bg2: '#303537',
     bg3: '#697379',
     bg4: '#373c3f',
   },
