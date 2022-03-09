49c86c285a8c3c28599cf78d3d66a8f771b3e716
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-09 19:18:14 +0900

    💄 focus ring

--- src/components/ui/TextInput/TextInput.tsx
+++ src/components/ui/TextInput/TextInput.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-native/no-inline-styles */
 import type { FC } from 'react';
-import React, { memo } from 'react';
+import React, { memo, useCallback, useRef, useState } from 'react';
 import { StyleSheet, TextInput as NativeTextInput } from 'react-native';
 
 import { View } from '~/components/ui/View';
@@ -30,27 +31,45 @@ export const TextInput: FC<TextInputProps> = memo(
     secureTextEntry = false,
     ...otherProps
   }) => {
+    const primary = useTheme({}, 'primary');
     const color = useTheme({ light: lightColor, dark: darkColor }, fontColor);
+
+    const inputRef = useRef(null);
+    const [isFocused, setIsFocused] = useState(false);
+    const onFocus = useCallback(() => setIsFocused(true), []);
+    const onBlur = useCallback(() => setIsFocused(false), []);
 
     return (
       <View
-        style={[defaultStyle.view, viewStyle]}
+        style={[
+          defaultStyle.view,
+          viewStyle,
+          isFocused && {
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 1,
+            elevation: 1,
+          },
+        ]}
+        border={isFocused ? 'primary' : border}
+        shadow={isFocused ? 'primary' : shadow}
         {...{
           bg,
           lightBg,
           darkBg,
-          border,
           lightBorder,
           darkBorder,
-          shadow,
           lightShadow,
           darkShadow,
         }}
       >
         <NativeTextInput
           {...otherProps}
+          ref={inputRef}
           secureTextEntry={secureTextEntry}
+          selectionColor={primary}
           style={[defaultStyle.text_input, textStyle, { color }]}
+          onPressOut={onFocus}
+          onBlur={onBlur}
         />
       </View>
     );
