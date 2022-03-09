cb8a99bb85df0e8745fa86b554ced0d3516dcb03
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-10 00:53:27 +0900

    💄 Change header style

--- src/screens/main/result/index.tsx
+++ src/screens/main/result/index.tsx
@@ -3,21 +3,39 @@ import type { FC } from 'react';
 import React from 'react';
 
 import { PrevButton } from '~/components/ui/Button';
+import { Text } from '~/components/ui/Text';
 import type { ResultStackParamList } from '~/types';
 
 import { ResultScreen } from './result.screen';
 import { ResultDetailScreen } from './result_detail.screen';
 
+const HeaderTitle: FC<{ title: string }> = ({ title }) => (
+  // eslint-disable-next-line react-native/no-inline-styles
+  <Text style={{ fontSize: 22, fontWeight: '600' }}>{title}</Text>
+);
+
 const Result = createNativeStackNavigator<ResultStackParamList>();
 
 export const ResultNavigator: FC = () => {
   return (
-    <Result.Navigator initialRouteName="ResultScreen">
+    <Result.Navigator
+      initialRouteName="ResultScreen"
+      screenOptions={{
+        headerTitleAlign: 'left',
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          fontSize: 18,
+          fontWeight: '600',
+        },
+      }}
+    >
       <Result.Screen
         name="ResultScreen"
         component={ResultScreen}
         options={() => ({
-          // headerShown: false,
+          title: '',
+          headerLayoutPreset: 'left',
+          headerLeft: () => <HeaderTitle title="大会の結果一覧" />,
         })}
       />
       <Result.Screen
@@ -26,9 +44,6 @@ export const ResultNavigator: FC = () => {
         options={({ navigation }) => ({
           title: '大会の結果',
           headerTitleAlign: 'left',
-          headerTitleStyle: {
-            fontWeight: '600',
-          },
           headerBackTitleVisible: false,
           headerLeft: () => {
             const onPrevScreen = () => navigation.goBack();
