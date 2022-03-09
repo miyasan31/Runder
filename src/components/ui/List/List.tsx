a88524c02ce5091508c0d261b2a19ba285c6b047
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-09 22:58:12 +0900

    ✨ List

---
+++ src/components/ui/List/List.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react-native/no-inline-styles */
+import type { FC, ReactNode } from 'react';
+import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+
+import { Bounceable, TouchableOpacity } from '~/components/ui/View';
+import type { CardStyleProps } from '~/types/style';
+
+export type CardProps = CardStyleProps & {
+  children: ReactNode;
+  isBorder?: true;
+  activeOpacity?: number;
+  onPress?: () => void;
+};
+
+export const List: FC<CardProps> = memo(
+  ({
+    // theme
+    bg = 'bg1',
+    lightBg,
+    darkBg,
+    border = 'border1',
+    shadow = 'shadow1',
+    // ViewProps
+    isBorder,
+    outlineStyle,
+    // TouchableOpacityProps
+    activeOpacity = 0.9,
+    viewStyle,
+    children,
+    onPress,
+    ...otherProps
+  }) => {
+    return (
+      <TouchableOpacity
+        style={[outlineStyle, isBorder && { borderBottomWidth: 1 }]}
+        {...{ bg, lightBg, darkBg, border, shadow, activeOpacity, otherProps }}
+        onPress={onPress}
+      >
+        <Bounceable viewStyle={[defaultStyle.bounceable_view, viewStyle]}>{children}</Bounceable>
+      </TouchableOpacity>
+    );
+  },
+);
+
+const defaultStyle = StyleSheet.create({
+  bounceable_view: {
+    paddingHorizontal: '1%',
+    paddingVertical: '2%',
+  },
+});
