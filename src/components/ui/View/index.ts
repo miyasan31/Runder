e00830147c8ae7832cbadf67e8e39f985ec12519
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-10 19:07:37 +0900

    ✨ ScrollView

--- src/components/ui/View/index.ts
+++ src/components/ui/View/index.ts
@@ -1,4 +1,5 @@
 export { Bounceable } from './Bounceable';
 export { SafeAreaView } from './SafeAreaView';
+export { ScrollView } from './ScrollView';
 export { TouchableOpacity } from './TouchableOpacity';
 export { View } from './View';
