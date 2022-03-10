f103a55ad8aa08a556a0a9ab3a7960e5336580ca
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-10 22:46:37 +0900

    💄 flatlist separator

--- src/styles/flatList.ts
+++ src/styles/flatList.ts
@@ -10,6 +10,12 @@ export const flatListStyle = StyleSheet.create({
     paddingBottom: 150,
     paddingHorizontal: '3%',
   },
+  separator: {
+    width: '94%',
+    marginHorizontal: '3%',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eaeaea',
+  },
   bottom_space_large: {
     paddingBottom: 150,
   },
