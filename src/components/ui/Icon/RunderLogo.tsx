531456be5f44e2a81ac5627c9ebcded98b94f9f4
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-13 14:26:48 +0900

    ✨ RunderLogo

---
+++ src/components/ui/Icon/RunderLogo.tsx
@@ -0,0 +1,197 @@
+import type { FC } from 'react';
+import React from 'react';
+import Svg, { ClipPath, Defs, G, LinearGradient, Path, Rect, Stop } from 'react-native-svg';
+
+export const RunderLogo: FC = () => {
+  return (
+    <Svg
+      width="40"
+      height="40"
+      viewBox="0 0 246 325"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <G clip-path="url(#clip0_545_10809)">
+        <Path
+          d="M215 94.87L8.2399 148.59L5.6499 134L213.4 86.77L215 94.87ZM216 99.87L9.8399 157.63L12.4399 172.26L217.44 108L216 99.87ZM218.41 113L14.0499 181.32L16.6499 196L220 121.15L218.41 113Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M79.79 172.67L214.65 243L221.34 252.28L80.41 182.28L79.79 172.67ZM81.57 200.26L233 268.38L225.71 258.26L80.83 188.76L81.57 200.26ZM83 222.07L245.65 286L237.65 274.92L82.08 208.07L83 222.07Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M200.54 89.6899L69.78 17.4199L70.59 29.9999L190.19 92.0499L200.54 89.6899Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M71.1399 38.6101L72.1499 54.1601L167.25 97.2601L182.51 93.7901L71.1399 38.6101Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M205.93 88.47L213.4 86.77L214.68 93.47L213.4 86.77L68.6499 0L69.3199 10.38L205.93 88.47Z"
+          fill="#19EC9C"
+        />
+        <Path d="M36.62 126.96L19.5 15.3701L0 21.4701L20.72 130.57L36.62 126.96Z" fill="#19EC9C" />
+        <Path
+          d="M58.3399 122.02L46.3499 6.96997L30.3799 11.97L45.4399 124.95L58.3399 122.02Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M76.2601 117.95L68.6501 0L55.3401 4.16L65.5801 120.38L76.2601 117.95Z"
+          fill="#19EC9C"
+        />
+        <Path d="M70.49 176.18L80.42 288.88L86.65 279L79.8 172.75L70.49 176.18Z" fill="#19EC9C" />
+        <Path
+          d="M32.0701 190.32L57.6501 325L65.1601 313.1L45.5701 185.35L32.0701 190.32Z"
+          fill="#19EC9C"
+        />
+        <Path
+          d="M53.1201 182.57L69.5801 306.09L76.4001 295.27L64.2201 178.49L53.1201 182.57Z"
+          fill="#19EC9C"
+        />
+      </G>
+      <Defs>
+        <LinearGradient
+          id="paint0_linear_545_10809"
+          x1="241.46"
+          y1="285.85"
+          x2="7.78003"
+          y2="151.37"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint1_linear_545_10809"
+          x1="69.78"
+          y1="54.7299"
+          x2="200.54"
+          y2="54.7299"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#15BE7D" />
+          <Stop offset="0.1" stop-color="#16CE88" />
+          <Stop offset="0.25" stop-color="#18DF93" />
+          <Stop offset="0.42" stop-color="#19E99A" />
+          <Stop offset="0.63" stop-color="#19EC9C" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint2_linear_545_10809"
+          x1="7994"
+          y1="4023.29"
+          x2="20397.3"
+          y2="4023.29"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#15BE7D" />
+          <Stop offset="0.1" stop-color="#16CE88" />
+          <Stop offset="0.25" stop-color="#18DF93" />
+          <Stop offset="0.42" stop-color="#19E99A" />
+          <Stop offset="0.63" stop-color="#19EC9C" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint3_linear_545_10809"
+          x1="10093.6"
+          y1="4368.79"
+          x2="31418.4"
+          y2="4368.79"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#15BE7D" />
+          <Stop offset="0.1" stop-color="#16CE88" />
+          <Stop offset="0.25" stop-color="#18DF93" />
+          <Stop offset="0.42" stop-color="#19E99A" />
+          <Stop offset="0.63" stop-color="#19EC9C" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint4_linear_545_10809"
+          x1="0"
+          y1="8421.51"
+          x2="1341.02"
+          y2="8421.51"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint5_linear_545_10809"
+          x1="879.805"
+          y1="7788.93"
+          x2="1661.57"
+          y2="7788.93"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint6_linear_545_10809"
+          x1="1213.05"
+          y1="7245.67"
+          x2="1650.7"
+          y2="7245.67"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint7_linear_545_10809"
+          x1="1209.61"
+          y1="26977.9"
+          x2="1470.75"
+          y2="26977.9"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint8_linear_545_10809"
+          x1="1093.27"
+          y1="35821.2"
+          x2="2188.21"
+          y2="35821.2"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <LinearGradient
+          id="paint9_linear_545_10809"
+          x1="1289.75"
+          y1="31094.7"
+          x2="1831.71"
+          y2="31094.7"
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stop-color="#19EC9C" />
+          <Stop offset="0.28" stop-color="#18E597" />
+          <Stop offset="0.69" stop-color="#17D18A" />
+          <Stop offset="1" stop-color="#15BE7D" />
+        </LinearGradient>
+        <ClipPath id="clip0_545_10809">
+          <Rect width="245.65" height="325" fill="white" />
+        </ClipPath>
+      </Defs>
+    </Svg>
+  );
+};
--- src/components/ui/Icon/index.ts
+++ src/components/ui/Icon/index.ts
@@ -5,3 +5,4 @@ export { Google } from './Google';
 export { Mail } from './Mail';
 export { MaterialIcon } from './MaterialIcon';
 export { Runder } from './Runder';
+export { RunderLogo } from './RunderLogo';
