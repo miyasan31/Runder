cdc35672656c87b6df1ffa2f3299d43094ce74d3
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-13 14:07:29 +0900

    ✨ SignInScreen

--- src/components/screen/SignIn/SignIn.tsx
+++ src/components/screen/SignIn/SignIn.tsx
@@ -1,6 +1,6 @@
 import type { FC } from 'react';
 import React, { useCallback } from 'react';
-import { Button as NativeButton, StyleSheet } from 'react-native';
+import { Button as NativeButton, Dimensions, Image, StyleSheet } from 'react-native';
 
 import { Button } from '~/components/ui/Button';
 import { Apple, Google, Mail, Runder } from '~/components/ui/Icon';
@@ -21,68 +21,93 @@ export const SignIn: FC<SignInScreenProps> = ({ navigation }) => {
   }, [navigation]);
 
   return (
-    <View style={layoutStyle.auth}>
-      <View style={style.icon_box}>
-        <Runder />
-      </View>
+    <>
+      <Image source={require('assets/images/runder-logo.png')} style={style.brand_logo} />
 
-      <View style={style.app_message_box}>
+      <View style={[style.float_box, layoutStyle.auth]}>
+        <View style={style.app_title_box}>
+          <Runder />
+        </View>
+
         <Text style={style.app_message}>あなたのライバルたちが待っています</Text>
         <Text style={style.app_message}>さっそく走り出しましょう</Text>
-      </View>
 
-      <Button
-        leftIcon={<Google />}
-        label="Googleでサインイン"
-        outlineStyle={style.button_outline}
-        viewStyle={style.button_bg}
-        textStyle={style.button_text}
-        bg="bg1"
-        onPress={onSignInGoogle}
-      />
-      <Button
-        leftIcon={<Apple />}
-        label="Appleでサインイン"
-        outlineStyle={style.button_outline}
-        viewStyle={style.button_bg}
-        textStyle={style.button_text}
-        lightBg="#333333"
-        lightColor="#FFF"
-        onPress={onSignOut}
-      />
-      <Button
-        leftIcon={<Mail />}
-        label="メールでサインイン"
-        outlineStyle={style.button_outline}
-        viewStyle={style.button_bg}
-        textStyle={style.button_text}
-        lightBg="#808080"
-        lightColor="#FFF"
-        onPress={onSignInEmailNavigate}
-      />
+        <View style={style.button_group}>
+          <Button
+            leftIcon={<Google />}
+            label="Googleでサインイン"
+            outlineStyle={style.button_outline}
+            viewStyle={style.button_bg}
+            textStyle={style.button_text}
+            bg="bg1"
+            lightBg="#FFF"
+            lightColor="#333"
+            darkBg="#FFF"
+            darkColor="#333"
+            onPress={onSignInGoogle}
+          />
+          <Button
+            leftIcon={<Apple />}
+            label="Appleでサインイン"
+            outlineStyle={style.button_outline}
+            viewStyle={style.button_bg}
+            textStyle={style.button_text}
+            lightBg="#333"
+            lightColor="#FFF"
+            darkBg="#333"
+            darkColor="#FFF"
+            onPress={onSignOut}
+          />
+          <Button
+            leftIcon={<Mail />}
+            label="メールでサインイン"
+            outlineStyle={style.button_outline}
+            viewStyle={style.button_bg}
+            textStyle={style.button_text}
+            lightBg="#808080"
+            lightColor="#FFF"
+            darkBg="#808080"
+            darkColor="#FFF"
+            onPress={onSignInEmailNavigate}
+          />
+        </View>
 
-      <View style={style.register_box}>
-        <Text style={style.register_text}>新規登録の場合は</Text>
-        <NativeButton title="こちら" onPress={onSignUpNavigate} />
+        <View style={style.register_box}>
+          <Text style={style.register_text}>サインアップは</Text>
+          <NativeButton title="こちら" onPress={onSignUpNavigate} />
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
 const style = StyleSheet.create({
-  icon_box: {
+  brand_logo: {
+    width: '100%',
+    height: '100%',
+    opacity: 0.5,
+  },
+  float_box: {
+    position: 'absolute',
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    width: Dimensions.get('window').width,
+  },
+  app_title_box: {
+    marginTop: '30%',
     alignItems: 'center',
     marginBottom: '6%',
   },
-  app_message_box: {
-    marginBottom: '40%',
-  },
   app_message: {
-    textAlign: 'center',
-    fontSize: 18,
+    marginVertical: '2%',
+    fontSize: 16,
     fontWeight: '600',
-    lineHeight: 28,
+    textAlign: 'center',
+  },
+
+  button_group: {
+    marginTop: '20%',
   },
   button_outline: {
     marginBottom: '3%',
@@ -94,6 +119,7 @@ const style = StyleSheet.create({
     width: 'auto',
     marginLeft: '4%',
   },
+
   register_box: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -102,7 +128,6 @@ const style = StyleSheet.create({
   register_text: {
     width: 'auto',
     fontSize: 18,
-    textAlign: 'center',
     marginRight: '-1%',
   },
 });
