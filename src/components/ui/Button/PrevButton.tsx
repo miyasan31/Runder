7f7b63fabae9244adbbef1668a1eca8a243eca11
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-15 01:39:24 +0900

    ✨ close

--- src/components/ui/Button/PrevButton.tsx
+++ src/components/ui/Button/PrevButton.tsx
@@ -2,20 +2,18 @@ import type { VFC } from 'react';
 import React, { memo } from 'react';
 import { StyleSheet } from 'react-native';
 
-import { FeatherIcon } from '~/components/ui/Icon';
+import { AntDesignIcon } from '~/components/ui/Icon';
 import { Bounceable } from '~/components/ui/View';
-import { useTheme } from '~/hooks/useTheme';
 
 type PrevProps = {
   onPress: () => void;
+  type: 'close' | 'left';
 };
 
-export const PrevButton: VFC<PrevProps> = memo(({ onPress }) => {
-  const icon = useTheme({}, 'icon');
-
+export const PrevButton: VFC<PrevProps> = memo(({ onPress, type = 'left' }) => {
   return (
     <Bounceable activeScale={0.8} viewStyle={style.bounceable_view} onPress={onPress}>
-      <FeatherIcon name="chevron-left" size={30} color={icon} />
+      <AntDesignIcon name={type} size={24} icon="icon" />
     </Bounceable>
   );
 });
