a950e57a812bf76bf3c789d98d143ecebed4acff
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-14 22:50:32 +0900

    🏷️ IconStyleProps

--- src/components/ui/Icon/AntDesignIcon.tsx
+++ src/components/ui/Icon/AntDesignIcon.tsx
@@ -3,13 +3,16 @@ import type { ComponentProps, FC } from 'react';
 import React, { memo } from 'react';
 
 import { useTheme } from '~/hooks/useTheme';
+import type { IconStyleProps } from '~/types/style';
 
-type Props = {
+type Props = IconStyleProps & {
   name: ComponentProps<typeof AntDesign>['name'];
   size?: ComponentProps<typeof AntDesign>['size'];
 };
 
-export const AntDesignIcon: FC<Props> = memo(({ size = 22, ...otherProps }) => {
-  const icon = useTheme({}, 'icon');
-  return <AntDesign {...otherProps} size={size} color={icon} />;
-});
+export const AntDesignIcon: FC<Props> = memo(
+  ({ icon = 'icon', lightIcon, darkIcon, size = 22, ...otherProps }) => {
+    const iconColor = useTheme({ light: lightIcon, dark: darkIcon }, icon);
+    return <AntDesign {...otherProps} size={size} color={iconColor} />;
+  },
+);
--- src/components/ui/Icon/MaterialIcon.tsx
+++ src/components/ui/Icon/MaterialIcon.tsx
@@ -3,13 +3,16 @@ import type { ComponentProps, FC } from 'react';
 import React, { memo } from 'react';
 
 import { useTheme } from '~/hooks/useTheme';
+import type { IconStyleProps } from '~/types/style';
 
-type Props = {
+type Props = IconStyleProps & {
   name: ComponentProps<typeof MaterialIcons>['name'];
   size?: ComponentProps<typeof MaterialIcons>['size'];
 };
 
-export const MaterialIcon: FC<Props> = memo(({ size = 22, ...otherProps }) => {
-  const icon = useTheme({}, 'icon');
-  return <MaterialIcons {...otherProps} size={size} color={icon} />;
-});
+export const MaterialIcon: FC<Props> = memo(
+  ({ icon = 'icon', lightIcon, darkIcon, size = 22, ...otherProps }) => {
+    const iconColor = useTheme({ light: lightIcon, dark: darkIcon }, icon);
+    return <MaterialIcons {...otherProps} size={size} color={iconColor} />;
+  },
+);
