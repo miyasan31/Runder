0b3a7c193034b930bffd4ab2cdec1a5bdb64e8aa
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-18 23:13:52 +0900

    🔧 commonTheme primaryA

--- src/styles/theme/common.theme.tsx
+++ src/styles/theme/common.theme.tsx
@@ -2,11 +2,13 @@ export const commonTheme = {
   light: {
     white: '#FFFFFF',
     primary: '#19EC9C',
+    primaryA: '#19ec9b30',
     accent: '#4BB4FF',
   },
   dark: {
     white: '#FFFFFF',
     primary: '#24D995',
+    primaryA: '#24D99530',
     accent: '#47ADF5',
   },
 } as const;
