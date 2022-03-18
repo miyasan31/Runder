1552630d1e751232ad486266ca59766a11d44b29
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-18 16:53:26 +0900

    ✨ fetching message

--- src/components/ui/Progress/ActivityIndicator.tsx
+++ src/components/ui/Progress/ActivityIndicator.tsx
@@ -2,15 +2,25 @@ import type { FC } from 'react';
 import React, { memo } from 'react';
 import { ActivityIndicator as NativeActivityIndicator, StyleSheet } from 'react-native';
 
+import { Text } from '~/components/ui/Text';
 import { View } from '~/components/ui/View';
 import { useTheme } from '~/hooks/useTheme';
 
-export const ActivityIndicator: FC = memo(() => {
+type Props = {
+  message?: string;
+};
+
+export const ActivityIndicator: FC<Props> = memo(({ message }) => {
   const primary = useTheme({}, 'primary');
 
   return (
     <View style={style.root} bg="bg1">
       <NativeActivityIndicator size="large" color={primary} />
+      {message && (
+        <Text style={style.message} color="color2">
+          {message}
+        </Text>
+      )}
     </View>
   );
 });
@@ -20,5 +30,10 @@ const style = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  message: {
+    marginTop: 10,
+    fontWeight: '600',
+    textAlign: 'center',
   },
 });
