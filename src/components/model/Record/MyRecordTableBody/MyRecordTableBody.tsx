283fe6176eb58216d80d7751487e89cc574a67d5
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-18 23:14:52 +0900

    ✨ CombatHistoryScreen

--- src/components/model/Record/MyRecordTableBody/MyRecordTableBody.tsx
+++ src/components/model/Record/MyRecordTableBody/MyRecordTableBody.tsx
@@ -1,27 +1,38 @@
+import { format } from 'date-fns';
 import type { FC } from 'react';
 import React from 'react';
 import { StyleSheet } from 'react-native';
 
 import { List } from '~/components/ui/List';
 import { Text } from '~/components/ui/Text';
+import { formatRecord } from '~/functions/formatRecord';
+import type { Record } from '~/types/model';
 
-const data = [
-  { date: '1/2', record: '10:00.0' },
-  { date: '1/13', record: '10:00.0' },
-  { date: '1/30', record: '10:00.0' },
-  { date: '1/21', record: '10:00.0' },
-  { date: '1/5', record: '10:00.0' },
-  { date: '1/22', record: '10:00.0' },
-];
+type Props = Record & {
+  index: number;
+  activeIndex: number;
+  onSelectRecord: (id: number) => void;
+};
 
-type RecordData = typeof data[0];
+export const MyRecordTableBody: FC<Props> = ({
+  index,
+  activeIndex,
+  record,
+  created_at,
+  onSelectRecord,
+}) => {
+  const date = format(new Date(created_at), 'M/d');
+  const recordResult = formatRecord(record);
 
-export const MyRecordTableBody: FC<RecordData> = ({ date, record }) => {
   return (
-    <List viewStyle={style.root}>
+    <List
+      viewStyle={style.root}
+      onPress={() => onSelectRecord(index)}
+      bg={index === activeIndex ? 'primaryA' : 'bg0'}
+    >
       <Text style={style.td_date}>{date}</Text>
 
-      <Text style={style.td_record}>{record}</Text>
+      <Text style={style.td_record}>{recordResult}</Text>
     </List>
   );
 };
