57180f5a3483021d925df3069892741c88234a6b
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-18 16:51:29 +0900

    🏷️ model tournament

---
+++ src/types/model.d.ts
@@ -0,0 +1,48 @@
+// export type Column =
+//   | 'tournament'
+//   | 'user'
+//   | 'point'
+//   | 'info'
+//   | 'location'
+//   | 'point_table'
+//   | 'record'
+//   | 'shoes'
+//   | 'suggestion';
+
+export type Term = 'Monthly' | 'Weekly' | 'Daily';
+
+export type Tournament = {
+  id: string;
+  name: string;
+  distance: number;
+  created_at: string;
+  start: string;
+  end: string;
+  image: string;
+  count: number;
+  rule: string;
+  term: number;
+  button_color: string;
+  button_edge_color: string;
+  button_image: string;
+  slide_color: string;
+};
+
+// const TOURNAMENT = [
+//   {
+//     id: '1',
+//     name: 'Winter Distance Challenge',
+//     distance: 3000,
+//     created_at: '2020-01-01',
+//     start: '2020-01-01',
+//     end: '2020-01-02',
+//     image: './assets/develop/tournament.jpeg',
+//     count: 10,
+//     rule: "It's a long way to go, but you'll get there.",
+//     term: 'Monthly',
+//     button_color: '',
+//     button_edge_color: '',
+//     button_image: '',
+//     slide_color: '',
+//   },
+// ];
