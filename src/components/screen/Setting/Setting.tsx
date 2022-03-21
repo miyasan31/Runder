88a443980c812c47691e203fe0358584acf4d5d4
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-22 01:11:25 +0900

    ✨ SettingScreen

--- src/components/screen/Setting/Setting.tsx
+++ src/components/screen/Setting/Setting.tsx
@@ -1,16 +1,97 @@
 import type { FC } from 'react';
 import React from 'react';
+import { StyleSheet } from 'react-native';
 
-import { Text } from '~/components/ui/Text';
+import { SettingList } from '~/components/screen/Setting/SettingList';
+import {
+  ChevronRightIcon,
+  ColorSwatchIcon,
+  LockClosedIcon,
+  MailIcon,
+  ShieldCheckIcon,
+  SparklesIcon,
+} from '~/components/ui/Icon';
 import { View } from '~/components/ui/View';
 import type { ProfileScreenProps } from '~/types';
+import { onSignOut } from '~/utils/supabase';
 
 export type SettingScreenProps = ProfileScreenProps<'SettingScreen'>;
 
-export const Setting: FC<SettingScreenProps> = () => {
+const SETTING_LIST_DATA = [
+  {
+    id: 'theme',
+    navigate: 'ThemeScreen',
+    leftText: 'テーマ',
+    leftComponent: <SparklesIcon icon="icon1" />,
+    rightComponent: <ChevronRightIcon />,
+    onPress: null,
+  },
+  {
+    id: 'privacy',
+    navigate: 'PrivacyScreen',
+    leftText: 'プライバシーポリシー',
+    leftComponent: <LockClosedIcon icon="icon1" />,
+    rightComponent: <ChevronRightIcon />,
+    onPress: null,
+  },
+  {
+    id: 'terms',
+    navigate: 'TermsScreen',
+    leftText: '利用規約',
+    leftComponent: <ShieldCheckIcon icon="icon1" />,
+    rightComponent: <ChevronRightIcon />,
+    onPress: null,
+  },
+  {
+    id: 'contact',
+    navigate: 'ContactScreen',
+    leftText: 'お問い合わせ',
+    leftComponent: <MailIcon icon="icon1" />,
+    rightComponent: <ChevronRightIcon />,
+    onPress: null,
+  },
+  {
+    id: 'version',
+    navigate: undefined,
+    leftText: 'アプリバージョン',
+    leftComponent: <ColorSwatchIcon icon="icon1" />,
+    rightText: '1.0.0',
+    isDivider: true,
+    onPress: null,
+  },
+  {
+    id: 'sign_out',
+    navigate: undefined,
+    leftText: 'サインアウト',
+    color: 'error',
+    onPress: () => onSignOut(),
+  },
+  {
+    id: 'account_delete',
+    navigate: 'AccountDeletionScreen',
+    leftText: 'アカウント削除',
+    rightComponent: <ChevronRightIcon />,
+    color: 'error',
+    onPress: null,
+  },
+] as const;
+
+export const Setting: FC<SettingScreenProps> = ({ navigation }) => {
   return (
-    <View>
-      <Text>SettingScreen</Text>
+    <View style={style.root}>
+      {SETTING_LIST_DATA.map((item: typeof SETTING_LIST_DATA[number]) => {
+        const onPress = item.onPress
+          ? item.onPress
+          : () => (item.navigate ? navigation.navigate(item.navigate) : null);
+        return <SettingList key={item.id} {...item} onPress={onPress} />;
+      })}
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  root: {
+    flex: 1,
+    paddingHorizontal: '4%',
+  },
+});
