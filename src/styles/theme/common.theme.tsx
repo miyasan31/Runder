accbf58f43776371034fbb7bd4a61c7edb78d0bb
miyasan_0301 <miyasan.19990301@gmail.com>
2022-03-22 01:12:47 +0900

    🔧 theme common: error

--- src/styles/theme/common.theme.tsx
+++ src/styles/theme/common.theme.tsx
@@ -4,11 +4,13 @@ export const commonTheme = {
     primary: '#19EC9C',
     primaryA: '#19ec9b30',
     accent: '#4BB4FF',
+    error: '#F83939',
   },
   dark: {
     white: '#FFFFFF',
     primary: '#24D995',
     primaryA: '#24D99530',
     accent: '#47ADF5',
+    error: '#F83939',
   },
 } as const;
