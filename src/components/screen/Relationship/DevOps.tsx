8137f8376dcaf077bc1bd5507fcf5495459f4fcc
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-22 09:15:32 +0900

    ✨ API: suggestion追加処理

--- src/components/screen/Relationship/DevOps.tsx
+++ src/components/screen/Relationship/DevOps.tsx
@@ -1,28 +1,45 @@
 import type { FC } from 'react';
 import React, { useCallback, useState } from 'react';
 import { StyleSheet } from 'react-native';
+import { useRecoilValue } from 'recoil';
 
 import { Button } from '~/components/ui/Button';
 import { Text } from '~/components/ui/Text';
 import { TextInput } from '~/components/ui/TextInput';
 import { View } from '~/components/ui/View';
 import { sleep } from '~/functions/sleep';
+import { user } from '~/stores/user';
 import { layoutStyle } from '~/styles';
+import { supabaseClient } from '~/utils/supabase';
 import { toastKit } from '~/utils/toastKit';
 
 import type { RelationshipScreenProps } from '.';
 
 export const DevOps: FC<RelationshipScreenProps> = () => {
+  const userInfo = useRecoilValue(user);
   const [text, setText] = useState('');
 
   const onChangeText = useCallback((value: string) => {
-    setText(value);
+    setText(value.trim());
   }, []);
 
   const onSubmit = useCallback(async () => {
+    if (!text) return;
+
     const { errorToast, successToast } = toastKit('送信しています');
-    await sleep(2000);
-    errorToast('送信に失敗しました');
+    const insertSuggestionPromise = supabaseClient.from('suggestion').insert({
+      user_id: userInfo?.user?.id,
+      contents: text,
+    });
+    const sleepPromise = sleep(400);
+    const [{ error }] = await Promise.all([insertSuggestionPromise, sleepPromise]);
+
+    if (error) {
+      errorToast('送信に失敗しました');
+      return;
+    }
+
+    setText('');
     successToast('送信しました');
   }, [text]);
 
