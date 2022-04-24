5ad44ce665e77d5025ed2a512937789feed57245
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-24 16:48:46 +0900

    ♻️ shoes state

--- src/stores/shoes.ts
+++ src/stores/shoes.ts
@@ -2,7 +2,11 @@ import { atom } from 'recoil';
 
 import type { Shoes } from '~/types/model';
 
-export const shoes = atom<Partial<Shoes>>({
+export const shoes = atom<Pick<Shoes, 'id' | 'brand' | 'shoes'>>({
   key: 'shoes',
-  default: {},
+  default: {
+    id: 0,
+    brand: '',
+    shoes: '',
+  },
 });
