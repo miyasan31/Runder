cfbeea31ea7d1ee3d97ffe7008673574dc03c450
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-26 09:23:42 +0900

    ✨ API: アバター更新

--- src/components/screen/AvatarEdit/AvatarEdit.tsx
+++ src/components/screen/AvatarEdit/AvatarEdit.tsx
@@ -6,17 +6,24 @@ import {
 import type { FC } from 'react';
 import React, { useCallback, useState } from 'react';
 import { Alert, Platform, StyleSheet } from 'react-native';
+import { useRecoilState } from 'recoil';
 
 import { Button } from '~/components/ui/Button';
 import { Image } from '~/components/ui/Image';
 import { View } from '~/components/ui/View';
+import { checkExtension } from '~/functions/checkExtension';
+import { sleep } from '~/functions/sleep';
+import { user } from '~/stores/user';
 import { layoutStyle } from '~/styles';
 import type { ProfileScreenProps as Props } from '~/types';
+import { supabaseClient } from '~/utils/supabase';
+import { toastKit } from '~/utils/toastKit';
 
 export type AvatarEditScreenProps = Props<'AvatarEditScreen'>;
 
-export const AvatarEdit: FC<AvatarEditScreenProps> = () => {
-  const [image, setImage] = useState<string | null>(null);
+export const AvatarEdit: FC<AvatarEditScreenProps> = ({ navigation }) => {
+  const [userInfo, setUserInfo] = useRecoilState(user);
+  const [avatarImageUri, setAvatarImageUri] = useState<string>('');
 
   const onPickImage = useCallback(async () => {
     if (Platform.OS !== 'web') {
@@ -28,25 +35,73 @@ export const AvatarEdit: FC<AvatarEditScreenProps> = () => {
     }
 
     const result = await launchImageLibraryAsync({
-      mediaTypes: MediaTypeOptions.All,
+      mediaTypes: MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
     });
 
     if (!result.cancelled) {
-      setImage(result.uri);
+      setAvatarImageUri(result.uri);
     }
   }, []);
+
+  const onAvatarUpload = useCallback(async () => {
+    const fileExtension = checkExtension(avatarImageUri);
+
+    const formData = new FormData();
+    formData.append('files', {
+      uri: avatarImageUri,
+      name: `${userInfo.id}.${fileExtension}`,
+      type: `image/${fileExtension}`,
+    });
+
+    const { errorToast, successToast } = toastKit();
+
+    const { error: fileUploadError } = await supabaseClient.storage
+      .from('public')
+      .upload(`avatar/${userInfo.id}.${fileExtension}`, formData, {
+        upsert: true,
+      });
+    if (fileUploadError) {
+      errorToast('アップロードに失敗しました');
+      return;
+    }
+
+    const { publicURL, error: publicUrlError } = supabaseClient.storage
+      .from('public')
+      .getPublicUrl(`avatar/${userInfo.id}.${fileExtension}`);
+    if (publicUrlError) {
+      errorToast('アップロードに失敗しました');
+      return;
+    }
+    if (!publicURL) {
+      errorToast('アップロードに失敗しました');
+      return;
+    }
+
+    const { error: userUpdateError } = await supabaseClient
+      .from('user')
+      .update({
+        avatar: publicURL,
+      })
+      .eq('id', userInfo.id);
+    if (userUpdateError) return;
+
+    successToast('アバターを更新しました');
+    setUserInfo((prevState) => {
+      return { ...prevState, avatar: publicURL };
+    });
+
+    await sleep(600);
+    navigation.navigate('ProfileScreen');
+  }, [avatarImageUri]);
 
   return (
     <View style={layoutStyle.modal}>
       <View style={style.align_horizontal}>
         <View bg="bg2" style={style.user_icon_box}>
-          <Image
-            source={image ? { uri: image } : require('assets/develop/lilnasx.png')}
-            style={style.user_icon}
-          />
+          <Image source={{ uri: avatarImageUri || userInfo.avatar }} style={style.user_icon} />
         </View>
 
         <Button
@@ -59,7 +114,13 @@ export const AvatarEdit: FC<AvatarEditScreenProps> = () => {
         />
       </View>
 
-      <Button label="保存する" bg="primary" color="white" outlineStyle={style.button_outline} />
+      <Button
+        label="保存する"
+        bg="primary"
+        color="white"
+        outlineStyle={style.button_outline}
+        onPress={onAvatarUpload}
+      />
     </View>
   );
 };
@@ -73,10 +134,6 @@ const style = StyleSheet.create({
   user_icon_box: {
     width: 'auto',
     padding: 10,
-  },
-  indicator: {
-    width: 120,
-    paddingVertical: 60,
   },
   user_icon: {
     width: 120,
