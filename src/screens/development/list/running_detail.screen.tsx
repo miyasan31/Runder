d4b847209fdb49788a032ba84e509df063a9cf83
miyasan_0301 <miyasan.19990301@gmail.com>
2022-04-30 03:07:38 +0900

    🔥

--- src/screens/development/list/running_detail.screen.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react';
-import React from 'react';
-
-import type { Props } from '~/components/screen/RunningDetail';
-import { RunningDetail } from '~/components/screen/RunningDetail';
-
-export const RunningDetailScreen: FC<Props> = (props) => {
-  return <RunningDetail {...props} />;
-};
--- src/screens/development/list/running_history.screen.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react';
-import React from 'react';
-
-import type { Props } from '~/components/screen/RunningHistory';
-import { RunningHistory } from '~/components/screen/RunningHistory';
-
-export const RunningHistoryScreen: FC<Props> = (props) => {
-  return <RunningHistory {...props} />;
-};
